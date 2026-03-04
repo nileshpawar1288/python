16df8877cc47f78d08ab4cbc0e07e69b43c47d85
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-04 15:34:30 +0200

    age calculator

---
+++ age_calculator.py
@@ -0,0 +1,8 @@
+print ("========age calculator======== ")
+
+current_age = int(input("Enter current age: "))
+future_years = int(input("Enter the number of years in the future: "))
+
+future_age = current_age + future_years
+
+print(f"\nIn {future_years} years, you will be {future_age} years old.")
