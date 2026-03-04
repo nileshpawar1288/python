d923039952554e945605e6d47c379cdcf3630470
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-04 16:55:23 +0200

    calculator

---
+++ Area_calculator.py
@@ -0,0 +1,6 @@
+print("==========Area Calculator======== ")
+
+radius = float(input("Enter the radius of the circle in meters: "))
+area = 3.14 * radius ** 2
+
+print(f"the area of the circle is {area} square meters.")
