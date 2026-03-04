ceb13a896646a932fc92782fd91fe0e26b42b0e6
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-04 16:03:39 +0200

    calculator

---
+++ age_calculator1.py
@@ -0,0 +1,10 @@
+print("========age calculator========")
+
+current_year = 2026
+
+birth_year = int(input("enter your birth year:"))
+
+age = current_year - birth_year
+
+print(f"you are {age} years old.")
+#the f before the string means formatting string it tell pythin "inside this string i will insert variables"
