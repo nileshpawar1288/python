7941a4e0408e4d1228a8900eb6b51dd1967184a8
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-04 15:13:31 +0200

    data types

---
+++ data_types.py
@@ -0,0 +1,17 @@
+# Basic data types
+
+age = 38               # int
+height = 5.7           # float
+name = "Nilesh"        # string
+is_learning = True     # boolean
+
+print(age)
+print(height)
+print(name)
+print(is_learning)
+
+# Check types
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_learning))
