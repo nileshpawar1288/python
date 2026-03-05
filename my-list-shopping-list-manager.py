f178fa4af9ba9dd128ff0252769422502dfef291
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-05 15:59:11 +0200

    manager

---
+++ my-list-shopping-list-manager.py
@@ -0,0 +1,22 @@
+#concept used
+#list
+#append()
+#modify element
+
+
+
+
+print("========Shopping List Manager========")
+shopping_list = ["milk", "bread", "eggs", "fruits", "vegetables"]
+
+print("shopping list:", shopping_list)
+
+shopping_list.append("butter")
+
+print("update list:", shopping_list)
+
+shopping_list[1] = "whole wheat bread"
+
+
+
+print("Final list:", shopping_list)
