90da475993f15568d038c48a7d174e9fd261b726
nilesh pawar <nilesh.pawar1288@gmail.com>
2026-03-06 15:51:41 +0200

    dict

---
+++ mydict-number-dictionary.py
@@ -0,0 +1,18 @@
+#creat a dictionary that maps numbers words to numbers.
+
+numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+
+}
+
+print(numbers)
+print("valu of one:", numbers["one"])
+print("valuse of three:", numbers["three"])
+numbers["six"] = 6,
+numbers["seven"] = 7,
+print(numbers.keys())
+print(numbers.values())
